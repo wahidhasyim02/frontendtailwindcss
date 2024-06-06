26730d8c82b10d5763564ce5856b9c17a1738d64
hidhi <wahidhasyim46@gmail.com>
2024-06-06 12:36:57 +0700

    hhdhd

--- src/script.js
+++ src/script.js
@@ -150,53 +150,11 @@ document.addEventListener("DOMContentLoaded", function () {
     popupChangepassword.classList.toggle("hidden");
   });
 });
-/*
-// Dark Mode
-document.addEventListener("DOMContentLoaded", function () {
-  var toggleDarkMode = document.getElementById("toggle-darkmode");
-  var textDarkMode = document.getElementById("text-darkmode");
-  var iconDarkMode = document.getElementById("icon-darkmode");
 
-  // Fungsi untuk mengatur tema
-  function setTheme(isDarkMode) {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-      textDarkMode.textContent = "Light Mode";
-      iconDarkMode.textContent = "light_mode";
-    } else {
-      document.documentElement.classList.remove("dark");
-      textDarkMode.textContent = "Dark Mode";
-      iconDarkMode.textContent = "dark_mode";
-    }
-  }
-
-  // Mendapatkan nama halaman saat ini
-  var currentPage = window.location.pathname.split("/").pop();
-
-  // Memeriksa nama halaman dan mengatur tema sesuai kebutuhan
-  if (currentPage === "login.html" || currentPage === "signup.html") {
-    // Selalu menggunakan mode terang untuk halaman login dan signup
-    setTheme(false);
-    // Menyimpan preferensi mode terang di local storage untuk halaman login dan signup
-    localStorage.setItem("darkMode", "false");
-  } else if (currentPage === "index.html") {
-    // Mengatur tema sesuai dengan yang tersimpan di local storage untuk halaman index
-    var isDarkMode = localStorage.getItem("darkMode") === "true";
-    setTheme(isDarkMode);
-  }
-
-  // Menambahkan event listener untuk toggle button
-  toggleDarkMode.addEventListener("click", function () {
-    var isDark = document.documentElement.classList.toggle("dark");
-    setTheme(isDark);
-    localStorage.setItem("darkMode", isDark ? "true" : "false");
-  });
-});*/
 
 document.addEventListener("DOMContentLoaded", function () {
   var toggleDarkMode = document.getElementById("toggle-darkmode");
-  var textDarkMode = document.getElementById("text-darkmode");
-  var iconDarkMode = document.getElementById("icon-darkmode");
+
 // Menambahkan event listener untuk toggle button
 toggleDarkMode.addEventListener("click", function () {
   var isDark = document.documentElement.classList.toggle("dark");
