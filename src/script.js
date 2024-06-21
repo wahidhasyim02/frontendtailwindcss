544d6ed5f0abe68b6ecea72614e1d7849adb9d30
wahidhasyim02 <wahidhasyim46@gmail.com>
2024-06-21 14:40:36 +0700

    memperbaiki script darkmode karena tidak menambahkan dark jika link tidak ada index.html

--- src/script.js
+++ src/script.js
@@ -188,18 +188,19 @@ document.addEventListener("DOMContentLoaded", function () {
     setTheme(false);
     // Menyimpan preferensi mode terang di local storage untuk halaman login dan signup
     localStorage.setItem("darkMode", "false");
-  } else if (currentPage === "index.html") {
-    // Mengatur tema sesuai dengan yang tersimpan di local storage untuk halaman index
+  } else {
+    // Mengatur tema sesuai dengan yang tersimpan di local storage untuk halaman lain
     var isDarkMode = localStorage.getItem("darkMode") === "true";
     setTheme(isDarkMode);
   }
 
-  // Menambahkan event listener untuk toggle button
-  toggleDarkMode.addEventListener("click", function () {
-    var isDark = document.documentElement.classList.toggle("dark");
-    setTheme(isDark);
-    localStorage.setItem("darkMode", isDark ? "true" : "false");
-  });
+  if (toggleDarkMode && textDarkMode && iconDarkMode) {
+    toggleDarkMode.addEventListener("click", function () {
+      var isDark = document.documentElement.classList.toggle("dark");
+      setTheme(isDark);
+      localStorage.setItem("darkMode", isDark ? "true" : "false");
+    });
+  }
 });
 
 // Script untuk update waktu
