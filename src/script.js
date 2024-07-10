62b229ee473424252ad03e12ee308b63e192cd9c
wahidhasyim02 <wahidhasyim46@gmail.com>
2024-07-10 19:03:36 +0700

    memperbaiki script

--- src/script.js
+++ src/script.js
@@ -471,7 +471,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
 });
 
-// Script Search
+// Script Search Menu
 document.addEventListener("DOMContentLoaded", (event) => {
   const searchInput = document.getElementById("search-menu");
   const listItems = document.querySelectorAll("#list-menu a");
@@ -489,24 +489,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-function toggleVisibility(buttonId, listId) {
-  const button = document.getElementById(buttonId);
-  const list = document.getElementById(listId);
+// Script Search Billing Cash
+document.addEventListener("DOMContentLoaded", (event) => {
+  const searchBillingCash = document.getElementById("search-billing-cash");
+  const listBillingCash = document.querySelectorAll("#body-billing-cash tr td");
 
-  button.addEventListener("click", function () {
-    list.classList.toggle("hidden");
+  searchBillingCash.addEventListener("input", function () {
+    const filterBillingCash = searchBillingCash.value.toLowerCase();
+
+    listBillingCash.forEach(function (itemValue) {
+      if (itemValue.textContent.toLowerCase().includes(filterBillingCash)) {
+        itemValue.classList.add("bg-yellow-50  dark:bg-yellow-50");
+      } else {
+        itemValue.classList.remove("bg-purple-50  dark:bg-purdark-800");
+      }
+    });
   });
-
-  document.addEventListener("click", function (event) {
-    if (!list.contains(event.target) && !button.contains(event.target)) {
-      list.classList.add("hidden");
-    }
-  });
-}
-
-toggleVisibility("btn-years", "list-years");
-toggleVisibility("btn-months", "list-months");
-toggleVisibility("btn-days", "list-days");
+});
 
 // Save profile changes
 document
@@ -526,7 +525,38 @@ document
     }, 6000);
   });
 
-//Fungsi checked all billing charge
+// Fungsi untuk mengubah visibilitas elemen action
+function toggleActionVisibility() {
+  const action = document.getElementById("action");
+  const checkboxes = document.querySelectorAll(
+    "#body_billing_charge .checkbox"
+  );
+  const headCheckbox = document.getElementById("checkbox-head");
+
+  let anyChecked =
+    headCheckbox.checked ||
+    Array.from(checkboxes).some((checkbox) => checkbox.checked);
+
+  if (anyChecked) {
+    action.classList.remove("hidden");
+  } else {
+    action.classList.add("hidden");
+  }
+}
+
+// Fungsi untuk menghapus status checked dari semua checkbox
+function uncheckAllCheckboxes() {
+  document.getElementById("checkbox-head").checked = false;
+  const checkboxes = document.querySelectorAll(
+    "#body_billing_charge .checkbox"
+  );
+  checkboxes.forEach(function (checkbox) {
+    checkbox.checked = false;
+  });
+  toggleActionVisibility(); // Update visibility of action after unchecking
+}
+
+// Event listener untuk checkbox-head
 document.getElementById("checkbox-head").addEventListener("click", function () {
   const checkboxes = document.querySelectorAll(
     "#body_billing_charge .checkbox"
@@ -536,4 +566,63 @@ document.getElementById("checkbox-head").addEventListener("click", function () {
   checkboxes.forEach(function (checkbox) {
     checkbox.checked = isChecked;
   });
+
+  toggleActionVisibility();
 });
+
+// Event listener untuk checkboxes dalam body_billing_charge
+document
+  .querySelectorAll("#body_billing_charge .checkbox")
+  .forEach(function (checkbox) {
+    checkbox.addEventListener("click", toggleActionVisibility);
+  });
+
+// Event listener untuk btn-go-action
+document.getElementById("btn-go-action").addEventListener("click", function () {
+  uncheckAllCheckboxes();
+  document.getElementById("action").classList.add("hidden");
+});
+
+// Event listener untuk btn-cancel-action
+document
+  .getElementById("btn-cancel-action")
+  .addEventListener("click", function () {
+    uncheckAllCheckboxes();
+    document.getElementById("action").classList.add("hidden");
+  });
+
+function toggleVisibility(buttonId, listId, textId) {
+  const button = document.getElementById(buttonId);
+  const list = document.getElementById(listId);
+  const text = document.getElementById(textId);
+
+  button.addEventListener("click", function () {
+    list.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !list.contains(event.target) &&
+      !button.contains(event.target) &&
+      !text.contains(event.target)
+    ) {
+      list.classList.add("hidden");
+    }
+  });
+
+  // Update button text when list item is clicked
+  const listItems = list.querySelectorAll("li");
+  listItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Update button text while keeping the icon intact
+      text.childNodes[0].nodeValue = this.innerText + " ";
+      list.classList.add("hidden");
+    });
+  });
+}
+
+// Initialize the toggle functionality for each button and list pair
+toggleVisibility("btn-years", "list-years", "text-years");
+toggleVisibility("btn-months", "list-months", "text-months");
+toggleVisibility("btn-days", "list-days", "text-days");
+toggleVisibility("btn-list-action", "list-actions", "text-action");
